94a1ea9b81bed1d3b2c5d80c2cc185942726c262
Tal Davidson <tal@guna.co.il>
2013-07-14 17:36:07 +0000

    modified web.js to read from index .html

--- web.js
+++ web.js
@@ -3,10 +3,19 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var fs = require('fs');
+    buf = new Buffer(256);
+    buf = fs.readFileSync("index.html");
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var fs = require('fs');
+buf = new Buffer(256);
+var infile = "index.html"
+buf = fs.readFileSync(infile);
+
